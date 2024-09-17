8402be4000b4864bc9f8f792d9e34a5750e814c7
AbdAlftahSalem <abd321alftah@gmail.com>
2024-09-17 10:19:51 +0300

    Add example code

--- example/build_post_man_collection_example.dart
+++ example/build_post_man_collection_example.dart
@@ -1,6 +1,5 @@
-// import 'package:build_post_man_collection/build_post_man_collection.dart';
-//
-// void main() {
-//   var awesome = Awesome();
-//   print('awesome: ${awesome.isAwesome}');
-// }
+import '../lib/features/build_api_collection/build_collections.dart';
+
+void main() {
+  BuildApiCollection.buildApiCollection();
+}
